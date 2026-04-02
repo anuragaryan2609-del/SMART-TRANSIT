81c237347625869ba7845b8c40bae13fdde4319a
ANURAGARYAN CHOUDHARY <anuragaryan2609@gmail.com>
2026-04-02 21:07:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function showMobile(){
-  document.getElementById("mobileBox").style.display = "block";
-  document.getElementById("emailBox").style.display = "none";
-}
-
-function showEmail(){
-  document.getElementById("mobileBox").style.display = "none";
-  document.getElementById("emailBox").style.display = "block";
-}
-
 function sendOTP(){
   let num = document.getElementById("mobile").value;
 
@@ -16,27 +6,9 @@ function sendOTP(){
     return;
   }
 
-  document.getElementById("otpBox").style.display = "block";
-}
+  alert("OTP Sent ✅ (Auto Login)");
 
-function verifyOTP(){
-  let otp = document.getElementById("otp").value;
-
-  if(otp.length == 5){
-    alert("Login Success ✅");
+  setTimeout(()=>{
     window.location.href = "home.html";
-  } else {
-    alert("Invalid OTP");
-  }
-}
-
-function loginEmail(){
-  let email = document.getElementById("email").value;
-
-  if(email.includes("@")){
-    alert("Login Success ✅");
-    window.location.href = "home.html";
-  } else {
-    alert("Invalid Email");
-  }
+  },1000);
 }
